f46e1b1166bcdfccad41ace215fc80b495f81745
AaBBcc0829aA <tigertom517@gmail.com>
2025-03-26 09:00:34 +0100

    header file for parameter fit

---
+++ ParametricFit.h
@@ -0,0 +1,96 @@
+#ifndef ParametricFit_h
+#define ParametricFit_h
+
+#include <vector>
+#include <map>
+#include <string>
+
+using namespace std;
+
+class PDF;
+class MultiPDF;
+
+class ParametricFit {
+  
+  public:
+    
+    enum mode {value, p_value, gauss, brute_force};
+    
+    class Func {
+      public:
+	Func(const string& n):
+	  name(n){}
+	virtual ~Func(){}
+	
+	string get_name() const{return name;}
+	virtual unsigned int n_fix() = 0;//number of fixed parameters
+	virtual unsigned int n_unk() = 0;//number of unknown parameters
+	virtual double f(double x, vector<double>* v_fix, vector<double>* v_unk) = 0;
+	//v_fix stands for fixed_parameters, v_unk for unknown_parameters
+	
+      private:
+	string name;
+    };
+    
+    ParametricFit(Func* g);
+    ParametricFit();
+    ~ParametricFit();
+    
+    void set_func(Func* g);
+    void set_misses(unsigned int i);
+    void add_fixed_parameter(PDF* p);
+    void delete_fixed_parameters();
+    void add_unknown_parameter(double min, double max, unsigned int steps,const string& name);
+    void delete_unknown_parameters();
+    void add_data(double x, PDF* y_PDF);
+    void set_data(vector<double>* xV, vector<PDF*>* yVP);
+    void delete_data();
+    void clear();
+    
+    void reject_missed_data(unsigned int i);//if a data is missed more than i times, it will be rejected
+    
+    MultiPDF* get_unknown_MultiPDF() const;
+    
+    double chi2(vector<double>* fix_par_values, vector<double>* unk_par_values) const;
+    double chi2() const;
+    unsigned int degrees_of_freedom() const;
+        
+    bool isready() const;
+    void set_min_value(double m);
+    
+    void fit(unsigned int n_rep, unsigned int seed, mode q);
+    
+    void print_misses(const string& filename) const;
+    void print_data(const string& filename) const;
+    void print_residuals(const string& filename) const;
+  
+    
+  private:
+    
+    ParametricFit(const ParametricFit& p);
+    ParametricFit& operator=(ParametricFit& p);
+    
+    Func* f;
+    
+    vector<PDF*>* fixed_parameters;
+    
+    bool extern_data_vectors;
+    unsigned int misses;
+    double min_value;
+    vector<double>* data_x;
+    vector<PDF*>* data_y_PDF;
+    vector<double>* data_y_means;
+    vector<double>* data_y_vars;
+    vector<unsigned int>* data_misses;
+     
+    //map<string,PDF*>* unknown_parameters_PDFs;
+    
+    MultiPDF* unknown_MultiPDF;
+    
+    
+    double data_iterator(vector<double>* v_fix, vector<double>* v_unk, mode q) const;
+    void fill_y_mv();
+  
+};
+
+#endif
